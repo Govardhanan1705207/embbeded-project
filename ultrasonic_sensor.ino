2bf4c4b9e79cb3dfc866c79020091f208c1f465a
Govardhanan1705207 <43169304+Govardhanan1705207@users.noreply.github.com>
2019-04-08 12:10:57 +0530

    Add files via upload

---
+++ ultrasonic_sensor.ino
@@ -0,0 +1,52 @@
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+int led_us=13;
+int buz=12;
+void setup() {
+  pinMode(led_us,OUTPUT);
+  pinMode(buz,OUTPUT);
+   Serial.begin(9600); // Starting Serial Terminal
+}
+
+void loop() {
+   long duration, inches, cm;
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   inches = microsecondsToInches(duration);
+   cm = microsecondsToCentimeters(duration);
+   
+   if(cm >= 15){
+    Serial.print("****NO VECHILE DETECTED******");
+   //Serial.print(inches);
+  // Serial.print("in, ");
+  // Serial.print(cm);
+   //Serial.print("cm");
+   Serial.println();
+    delay(1000);
+   }
+   else{
+     Serial.print("*************DANGER ALERT*******");
+     Serial.println();                                                                          
+     digitalWrite(led_us, HIGH);
+     digitalWrite(buz,HIGH);
+     delay(100);
+     digitalWrite(led_us,LOW);
+     digitalWrite(buz,LOW);
+     delay(100);
+   }
+   
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
